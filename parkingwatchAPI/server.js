3ab8ed5a5b7dba3eb545e9edaf6a9e297eb5f832
olinator98 <oliver.werlen@hevs.ch>
2018-06-20 10:06:53 +0000

    removed unnecessary stuff server.js

--- parkingwatchAPI/server.js
+++ parkingwatchAPI/server.js
@@ -1,31 +1,3 @@
-/*var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Parkingwatch = require('./api/models/parkingwatchapiModel'), //created model loading here
-  bodyParser = require('body-parser');
-  
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:3edc4rfv@ds163870.mlab.com:63870/rest_api_to_test');
-
-var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/parkingwatchapiRoutes'); //importing route
-routes(app); //register the route
-
-
-app.listen(port);
-
-
-console.log('todo list RESTful API server started on: ' + port);
-*/
 
 var express = require('express'),
   app = express(),
